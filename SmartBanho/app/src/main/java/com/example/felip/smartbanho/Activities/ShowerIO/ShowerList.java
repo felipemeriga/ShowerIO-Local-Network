eb5d8b76396ea75d91c2796a695864f6e22b4afd
Felipe <felipe.meriga@gmail.com>
2018-09-04 19:54:52 -0300

    Starting the new points of ShowerList activity, where will list all the shower devices that have been found

---
+++ SmartBanho/app/src/main/java/com/example/felip/smartbanho/Activities/ShowerIO/ShowerList.java
@@ -0,0 +1,15 @@
+package com.example.felip.smartbanho.Activities.ShowerIO;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.felip.smartbanho.R;
+
+public class ShowerList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shower_list);
+    }
+}
