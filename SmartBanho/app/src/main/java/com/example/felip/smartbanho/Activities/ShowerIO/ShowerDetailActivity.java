fbb2094115965fd01bb75129a61aaf038e979ee2
Felipe <felipe.meriga@gmail.com>
2018-09-10 21:19:49 -0300

    Basic files and templates for ShowerDetailActivity

--- SmartBanho/app/src/main/java/com/example/felip/smartbanho/Activities/ShowerIO/ShowerDetailActivity.java
+++ SmartBanho/app/src/main/java/com/example/felip/smartbanho/Activities/ShowerIO/ShowerDetailActivity.java
@@ -1,15 +1,49 @@
 package com.example.felip.smartbanho.Activities.ShowerIO;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.support.v7.widget.CardView;
 
 import com.example.felip.smartbanho.R;
 
 public class ShowerDetailActivity extends AppCompatActivity {
 
+    GridLayout mainGrid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shower_detail);
+
+        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
+
+        //Set Event
+        setSingleEvent(mainGrid);
+        //setToggleEvent(mainGrid);
+    }
+
+
+
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    /*Intent intent = new Intent(MainActivity.this,ActivityOne.class);
+                    intent.putExtra("info","This is activity from card item index  "+finalI);
+                    startActivity(intent);*/
+
+                }
+            });
+        }
     }
 }
